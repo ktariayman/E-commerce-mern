703a6a2b9b3f0258aef74afc414457a233a8aac3
ktariayman <aymanktari31@gmail>
2022-04-05 01:50:03 +0100

    Fetch Products From Backend

--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
+import { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
-
+// import data from '../data';
+import axios from 'axios'
 function HomeScreen() {
+  const[products,setProduct] = useState([])
+  useEffect(() => {
+    const fetchData = async () =>{
+      const result =await axios.get('/api/products')
+      setProduct(result.data)
+    }
+    fetchData()
+  },[]);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
